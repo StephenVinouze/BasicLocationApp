62b4cb5e57cf0b33dbecea158ffc9a980bb4e24a
Stephen Vinouze <s.vinouze@gmail.com>
2015-10-09 00:53:29 +0200

    [~] Rename activity

--- app/src/main/java/com/stephenvinouze/basiclocationapp/activities/MainActivity.java
+++ app/src/main/java/com/stephenvinouze/basiclocationapp/activities/MainActivity.java
@@ -4,7 +4,6 @@ import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -20,8 +19,8 @@ import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.EActivity;
 
-@EActivity(R.layout.activity_maps)
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+@EActivity(R.layout.main_activity)
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
 
     @Bean
     KBLocationProvider mLocationProvider;
@@ -38,15 +37,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private void configureMap(Location location) {
         if (location != null) {
-            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
             CameraPosition cp = new CameraPosition.Builder()
+                    .target(new LatLng(location.getLatitude(), location.getLongitude()))
                     .bearing(location.getBearing())
                     .tilt(0)
-                    .target(latLng)
                     .zoom(16)
                     .build();
-            CameraUpdate cu = CameraUpdateFactory.newCameraPosition(cp);
-            mMapFragment.getMap().animateCamera(cu);
+            mMapFragment.getMap().animateCamera(CameraUpdateFactory.newCameraPosition(cp));
         }
     }
 
@@ -76,7 +73,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                 @Override
                 public void onLocationFailed() {
-                    Toast.makeText(MapsActivity.this, "Failed to fetch current location", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, getString(R.string.location_error), Toast.LENGTH_SHORT).show();
                 }
             });
         }
