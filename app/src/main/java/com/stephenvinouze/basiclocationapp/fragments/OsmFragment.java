2c2b1f867232501aac8223d80b79382bccf3b451
Stephen Vinouze <s.vinouze@gmail.com>
2015-10-11 14:42:51 +0200

    [+] Display OSM

--- app/src/main/java/com/stephenvinouze/basiclocationapp/fragments/OsmFragment.java
+++ app/src/main/java/com/stephenvinouze/basiclocationapp/fragments/OsmFragment.java
@@ -4,11 +4,43 @@ import android.support.v4.app.Fragment;
 
 import com.stephenvinouze.basiclocationapp.R;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.ViewById;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 /**
  * Created by Stephen Vinouze on 10/10/2015.
  */
 @EFragment(R.layout.osm_layout)
 public class OsmFragment extends Fragment {
+
+    @ViewById(R.id.mapview)
+    MapView mMapView;
+
+    private MyLocationNewOverlay mOverlay;
+
+    @AfterViews
+    void initViews() {
+        mMapView.getController().setZoom(20);
+        mOverlay = new MyLocationNewOverlay(getActivity(), mMapView);
+        mMapView.getOverlays().add(mOverlay);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        mOverlay.enableMyLocation();
+        mOverlay.enableFollowLocation();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        mOverlay.disableMyLocation();
+        mOverlay.disableFollowLocation();
+    }
 }
